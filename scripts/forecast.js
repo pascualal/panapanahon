f41ab521dc4bcd5111c396002ff3beb89c118b6f
alpascual <brianaldin.pascual@gmail.com>
2019-10-13 13:26:33 +0800

    weather panahon

---
+++ scripts/forecast.js
@@ -0,0 +1,38 @@
+const key = 'sQs6Ylk8S1BtIZJHDv8pXGVMLoC2j0Pe';
+
+// get weather information
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+ 
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+//   console.log(data)
+  return data[0];
+
+};
+
+getCity("manila")
+    .then(data => {
+        return getWeather(data.Key)
+    }).then(data => {
+        console.log(data)
+    })
+    .catch(err => console.log(err));
+
